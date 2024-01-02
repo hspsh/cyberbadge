571c65206e29fba3c541229f6702e0818b139377
CritBit <Jakub.Sadowski1@gmail.com>
2024-01-02 16:26:44 +0100

    put stuff for article etc.

---
+++ 2_0/ESP-shifters-FW/include/pinDefs.h
@@ -0,0 +1,31 @@
+
+
+#pragma once
+
+const int BUTTON_PIN_1 = 0;
+const int BUTTON_PIN_2 = 26;
+const int BUTTON_PIN_3 = 25;
+
+const int PIN_WS2812 = 22;
+
+
+
+
+const int DATA_PIN =  13;
+const int CLOCK_PIN =  22;
+const int LATCH_PIN =  23;
+const int OE_PIN =  19;
+
+const int ROW1_PIN =  15;
+const int ROW2_PIN =  17;
+const int ROW3_PIN =  0;
+const int ROW4_PIN =  16;
+const int ROW5_PIN =  4;
+const int ROW6_PIN =  12;
+const int ROW7_PIN =  18;
+const int ROW8_PIN =  5;
+
+const int ROW_PINS[8] = {
+    ROW1_PIN, ROW2_PIN, ROW3_PIN, ROW4_PIN,
+    ROW5_PIN, ROW6_PIN, ROW7_PIN, ROW8_PIN
+};
--- 2_0/ESP-shifters-FW/src/main.cpp
+++ 2_0/ESP-shifters-FW/src/main.cpp
@@ -1,72 +1,118 @@
-#define DATA_PIN 13
-#define CLOCK_PIN 22
-#define LATCH_PIN 23
-
-#define OE_PIN 19
-#define ROW1_PIN 15
-#define ROW2_PIN 2
-#define ROW3_PIN 0
-#define ROW4_PIN 16
-#define ROW5_PIN 4
 
 #include <Arduino.h>
+#include "pinDefs.h"
 
 
-void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t val)
-{
-  uint8_t i;
+class BootlegDumbMatrix {
+public:
 
-  for (i = 0; i < 8; i++)  {
-    digitalWrite(dataPin, !!(val & (1 << i)));
-    
-    digitalWrite(clockPin, HIGH);
-    digitalWrite(clockPin, LOW);
+  int oe;
+  int latch;
+  int clock;
+  int data;
+
+  int rowCount;
+  int *rowPins;
+
+  BootlegDumbMatrix(){};
+
+  void init(){
+    pinMode(oe, OUTPUT);
+    pinMode(latch, OUTPUT);
+    pinMode(clock, OUTPUT);
+    pinMode(data, OUTPUT);
+
+    for (int i = 0; i < rowCount; i++) {
+      pinMode(rowPins[i], OUTPUT);
+      digitalWrite(rowPins[i], HIGH);
+    }
+
+    digitalWrite(oe, LOW);
   }
-}
 
-int pins[] = {ROW1_PIN, ROW2_PIN, ROW3_PIN, ROW4_PIN, ROW5_PIN};
-
-
-void setRow(int row) {
-   for (int i = 0; i < 5; i++) {
-    digitalWrite(pins[i], HIGH);
-  }
-  if (row > 4) {
+  void setRow(int row) {
+    for (int i = 0; i < rowCount; i++) {
+      digitalWrite(rowPins[i], HIGH);
+    }
+    if (row > rowCount) {
       return;
+    }
+    digitalWrite(rowPins[row], LOW);
   }
-  digitalWrite(pins[row], LOW);
-}
 
+  void shiftOut(uint8_t val) {
+    uint8_t i;
+
+    for (i = 0; i < 8; i++)  {
+      digitalWrite(data, !!(val & (1 << i)));
+      
+      digitalWrite(clock, HIGH);
+      digitalWrite(clock, LOW);
+    }
+  }
+
+  uint8_t lineBuffer[12];
+
+  void spewBuf(){
+    for (int i = 0; i < 12; i++) {
+      shiftOut(lineBuffer[i]);
+    }
+  }
+
+  void drawRow(int rowNum){
+    setRow(rowNum);
+
+
+    delayMicroseconds(delayVal);
+    digitalWrite(LATCH_PIN, LOW);
+    // delay(1);
+    digitalWrite(LATCH_PIN, HIGH);
+  }
+
+  void drawFrame(){
+    for (int line = 0; line < 8; line++) {
+      uint8_t *pattern = getPattern(line);
+      for (int i = 0; i < 12; i++) {
+        lineBuffer[i] = pattern[i];
+      }
+      spewBuf();
+    }
+  }
+
+  uint8_t getGreenPattern(int i){
+    switch (i%3){
+      case 0:
+        return 0b01001001;
+      case 1:
+        return 0b10010010;
+      default:
+        return 0b00100100;
+    }
+  }
+
+};
+
+BootlegDumbMatrix dmatrix = BootlegDumbMatrix();
 
 void setup() {
-  pinMode(DATA_PIN, OUTPUT);
-  pinMode(CLOCK_PIN, OUTPUT);
-  pinMode(LATCH_PIN, OUTPUT);
-  pinMode(OE_PIN, OUTPUT);
-  pinMode(ROW1_PIN, OUTPUT);
-  pinMode(ROW2_PIN, OUTPUT);
-  pinMode(ROW3_PIN, OUTPUT);
-  pinMode(ROW4_PIN, OUTPUT);
-  pinMode(ROW5_PIN, OUTPUT);
 
-  digitalWrite(OE_PIN, LOW);
+  dmatrix.oe = OE_PIN;
+  dmatrix.latch = LATCH_PIN;
+  dmatrix.clock = CLOCK_PIN;
+  dmatrix.data = DATA_PIN;
+  dmatrix.rowCount = 8;
+  dmatrix.rowPins = new int[8];
+  memcpy(dmatrix.rowPins, ROW_PINS, 8*sizeof(int));
+  // dmatrix.rowPins = ROW_PINS;
+  dmatrix.init();
+
+  Serial.begin(115200);
 }
 
 int counter = 0;
+int delayVal = 15000;
 
 void loop() {
-  setRow(counter++);
-  if (counter > 5) {
-    delay(5);
-    counter = 0;
-  }
-  for (int i = 0; i < 96; i++) {
-    shiftOut(DATA_PIN, CLOCK_PIN, 1);
-    digitalWrite(LATCH_PIN, LOW);
-    // delay(1);
-    delayMicroseconds(10);
-    digitalWrite(LATCH_PIN, HIGH);
-    // delay(2);
-  }
-
+  dmatrix.drawFrame();
+  
 }
